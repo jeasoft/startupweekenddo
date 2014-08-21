c1af9ae6d1ed77d0ac74a2db7d3fffbc69b0ed37
Vivian Guillen <vivian@localhost.localdomain>
2014-08-21 15:06:20 -0400

    added menu, acerca, programa and collaborator

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,1 +1,41 @@
 /* Project specific Javascript goes here. */
+
+
+
+$(document).ready (function () {
+
+	// Cover Full Screen
+
+	$(window).resize(window_resize);
+
+	window_resize();
+
+	function window_resize() {
+		var height = $(window).height();
+
+		$('#cover').height( height );
+	}
+
+	$(window).scroll(function (event){
+		var top = $(window).scrollTop();
+		var start = $("#cover").position().top + $("#cover").height(); 
+
+		if (top > start) {
+			$("#home-navbar").css({
+				position: "fixed",
+				top: 0,
+				width: '100%'
+			});
+		} else {
+			$("#home-navbar").css("position", "inherit");
+		}
+
+
+	});
+
+	$("#scroll").click(function() {
+	    $('html, body').animate({
+	        scrollTop: $("#home-navbar").offset().top
+	   		}, 1000);
+		});
+})
