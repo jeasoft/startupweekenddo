f3cff89dff4485a8e65eda8736649aa2cdc7cc69
Vivian Guillen <vivianguillen@Vivians-MacBook-Air.local>
2014-08-25 10:40:11 -0400

    fixed navbar issue

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -16,27 +16,31 @@ $(document).ready (function () {
 		$('#cover').height( height );
 	}
 
-	$(window).scroll(function (event){
-		var top = $(window).scrollTop();
-		var start = $("#cover").position().top + $("#cover").height(); 
+	// $(window).scroll(function (event){
+	// 	var top = $(window).scrollTop();
+	// 	var start = $("#cover").position().top + $("#cover").height(); 
 
-		if (top > start) {
-			$("#home-navbar").css({
-				position: "fixed",
-				top: 0,
-				width: '100%'
-			});
-		} else {
-			$("#home-navbar").css("position", "inherit");
-		}
-
-
-	});
+	// 	if (top > start) {
+	// 		$("#home-navbar").css({
+	// 			position: "fixed",
+	// 			top: 0,
+	// 			width: '100%'
+	// 		});
+	// 	} else {
+	// 		$("#home-navbar").css("position", "inherit");
+	// 	}
+	// });
 
 	$("#more-button").click(function() {
 	    $('html, body').animate({
 	        scrollTop: $("#home-navbar").offset().top
 	   		}, 1000);
 		});
+
+	$('#nav-wrapper').height($("#home-navbar").height());
+    
+    $('#home-navbar').affix({
+        offset: { top: $('#home-navbar').offset().top }
+    });
 
 });
