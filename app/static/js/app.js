14001a48728961c68093c5dabc956f4ccb1bc4f3
Vivian Guillen <vivianguillen@Vivians-MacBook-Air.local>
2014-09-22 15:46:42 -0400

    added scrolldown buttons

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -15,11 +15,39 @@ $(document).ready (function () {
         });
     });
 
+    // Scroll Down Buttons
+
 	$("#more-button").click(function() {
 	    $('html, body').animate({
 	        scrollTop: $("#home-navbar").offset().top
 	   		}, 1000);
 		});
+
+    $("#acerca").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#about").offset().top
+            }, 1000);
+        });
+
+    $("#programa").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#program").offset().top
+            }, 1000);
+        });
+
+    $("#patrocinadores").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#sponsors").offset().top
+            }, 1000);
+        });
+
+    $("#colaboradores").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#facilitator").offset().top
+            }, 1000);
+        });
+
+    // Navbar affix
 
 	$('#nav-wrapper').height($("#home-navbar").height());
     
